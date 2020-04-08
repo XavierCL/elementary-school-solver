1ad9c5cbfddd92ab06a136abbd0f66525aff7444
Xavier <xavierx-men@hotmail.com>
2020-04-08 00:09:56 -0400

    remvoed a commented line

--- Simulated annealing/main.py
+++ Simulated annealing/main.py
@@ -3,6 +3,5 @@ from solver import getSolutionInstance
 
 if __name__ == "__main__":
     class1 = frankClasses.frankClassesInstance()
-    # solutionInstance = getSolutionInstance(class1, 1_000_000_000_000, 4, 0.9965)
     solutionInstance = getSolutionInstance(class1, 1_000_000_000_000, 4, 0.9965)
     print(solutionInstance.toString())
