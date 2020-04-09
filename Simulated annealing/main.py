fc54238e56c6b58a2dfafe1e1e45d28b95458a27
Xavier <xavierx-men@hotmail.com>
2020-04-08 20:28:04 -0400

    Fixed some stuff

--- Simulated annealing/main.py
+++ Simulated annealing/main.py
@@ -4,7 +4,5 @@ from solver import getSolutionInstance
 if __name__ == "__main__":
     ecole_de_Frank = frankClasses.frankClassesInstance()
     ecole_Isabelle = IsaBaronClasses.isaBaronClassesInstance()
-#    solutionInstance = getSolutionInstance(class1, 1_000_000_000_000, 4, 0.9965)
-#    solutionInstance = getSolutionInstance(ecole_de_Frank, 60_000, 15, 0.99765)
     solutionInstance = getSolutionInstance(ecole_Isabelle, 60_000, 15, 0.99765)
     print(solutionInstance.toString())
